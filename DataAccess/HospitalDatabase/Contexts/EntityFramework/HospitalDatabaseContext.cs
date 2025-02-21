981865376c733205990edcfd2ceb4b0bb7ebb78d
ekulac <kulacedanur460@gmail.com>
2025-02-21 14:02:11 +0300

    Add project files.

---
+++ DataAccess/HospitalDatabase/Contexts/EntityFramework/HospitalDatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿using Domain_one.HospitalDatabase.Tables;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess_two.HospitalDatabase.Contexts.EntityFramework
+{
+    public class HospitalDatabaseContext:DbContext 
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = .; Database = HospitalManagement; User Id = sa; Password = 1; TrustServerCertificate=True;");
+
+        }
+
+        #region Tables
+        public DbSet<Firm> Firm { get; set; }
+        #endregion
+    }
+}
---
+++ Domain/HospitalDatabase/Tables/Base/BaseEntity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain_one.AC.Abstract;
+
+
+
+namespace Domain_one.HospitalDatabase.Tables.Base
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class BaseEntity:IEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public DateTime CreateTime { get; set; }
+        public DateTime? UpdateTime { get; set; }
+        public DateTime? DeleteTime { get; set; }
+        public bool Deleted { get; set; }
+       
+     
+    }
+}
